9debdcb0c6c27f8b72a43cba1c589132988a0550
ming <ming@mingui-MacBookAir.local>
2026-05-17 19:53:47 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
+//메인페이지 
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 상단 메뉴 만들기 */}
+      <div>
+        <h4>블로그임</h4>
+      </div>
     </div>
   );
 }
